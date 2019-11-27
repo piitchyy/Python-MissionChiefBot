447ea7ec8815e6f1895f23b2aa382eb0583ced52
Linus Karlsson <liinuz@live.se>
2019-11-28 00:26:44 +0100

    Moved all settings into config.ini

--- missionchief_bot.py
+++ missionchief_bot.py
@@ -1,4 +1,5 @@
 from splinter import Browser
+from configparser import ConfigParser
 import time
 import platform
 import random
@@ -6,10 +7,15 @@ import os
 operatingsystem = platform.system()
 hrefs= []
 path = os.path.dirname(os.path.realpath(__file__))
- 
-# Get URL from file
-with open(path + "/url.txt", 'r') as f:
-    baseurl = f.readline().strip()
+config = ConfigParser()
+
+# Get config values
+config.read('config.ini')
+baseurl = config.get('Settings', 'URL')
+username = config.get('Settings', 'Username')
+password = config.get('Settings', 'Password')
+Min_Sleeptime = config.getint('Settings', 'Min_Sleeptime')
+Max_Sleeptime = config.getint('Settings', 'Max_Sleeptime')
 
 class MissonChiefBot:
  def init(self):
@@ -18,10 +24,10 @@ class MissonChiefBot:
      while True:
       hrefs.clear()
       getMissions()
-      timer = random.randint(30,60)
+      timer = random.randint(Min_Sleeptime,Max_Sleeptime)
       print('Sleeping for',timer,'seconds...')
       time.sleep(timer)
-    else: 
+    else:
      print("Couldn't log in...")
 def login(username,password):
     print("Logging in")
@@ -33,7 +39,7 @@ def login(username,password):
     browser.fill("user[password]",password)
     # Submitting login
     browser.find_by_name('commit').click()
-    try : 
+    try :
      # check we are logged in- by grabbing a random tag only visible on log in.
      alliance = browser.find_by_id('alliance_li')
      print("Logged in")
@@ -41,7 +47,7 @@ def login(username,password):
       return True
      else:
       return False
-    except Exception: 
+    except Exception:
      return False
 
 def getMissions():
@@ -52,7 +58,7 @@ def getMissions():
     try:
         links = browser.find_link_by_partial_href('/missions/')
         print(str(len(links)) + " missons found")
-        for link in links: 
+        for link in links:
          hrefs.append(link['href'])
         doMissions()
         return True;
@@ -62,8 +68,8 @@ def getMissions():
 def doMissions():
  count = 0
  for href in hrefs:
-  time.sleep(5)   
-  count+=1  
+  time.sleep(5)
+  count+=1
   mission_str = str(count)
   try:
    print("MISSION " + mission_str +":" + " VISITING MISSION")
@@ -71,24 +77,17 @@ def doMissions():
   except:
    print("MISSION " + mission_str +":" + " COULDN'T GET LINK")
   try:
-   print("MISSION " + mission_str +":" + " SELECTING UNIT TO DESPATCH")   
+   print("MISSION " + mission_str +":" + " SELECTING UNIT TO DESPATCH")
    checkbox=browser.find_by_css('input[class="vehicle_checkbox"]')
    for check in checkbox:
     check.check()
   except:
-   print("MISSION " + mission_str +":" + " NO UNITS TO DESPATCH")   
+   print("MISSION " + mission_str +":" + " NO UNITS TO DESPATCH")
   try:
    browser.find_by_name('commit').click()
-   print("MISSION " + mission_str +":" + " ATTEMPTED TO DESPATCH.")   
+   print("MISSION " + mission_str +":" + " ATTEMPTED TO DESPATCH.")
   except:
    print("MISSION " + mission_str +":" + "CAN NOT DESPATCH A UNIT, OR UNIT ALREADY DESPATCHED")
-
-    
-# Taking account information from file
-with open(path + '/account.txt', 'r') as f:
-    username = f.readline().strip()
-    password = f.readline().strip()
-    
 
 # Setting up browser
 if operatingsystem == "Windows":
@@ -98,10 +97,10 @@ elif operatingsystem == "Linux":
 
 elif operatingsystem == "Darwin":
   executable_path = {'executable_path': path+'/mac/chromedriver'}
- 
+
 browser = Browser('chrome', **executable_path)
 
-def begin(): 
+def begin():
  MissonChiefBot().init()
 
 if __name__ == '__main__':
